77956247ebe782c2085c5ffb9bfc36f8a9ccbc47
zzblueteamzz27 <zzblueteamzz27@gmail.com>
2022-05-09 21:13:11 +0300

    I make  2 of my Controllers

---
+++ HospitalProject/Controllers/SpecialitiesController.cs
@@ -0,0 +1,151 @@
+﻿using Data.Entities;
+using Data.HospitalCountext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalProject.Controllers
+{
+    public class SpecialitiesController : Controller
+    {
+           private readonly HospitalContext _context;
+            public SpecialitiesController(HospitalContext context)
+            {
+                _context = context;
+            }
+
+            // GET: Roles
+            public async Task<IActionResult> Index()
+            {
+               // ViewBag.UserRole = UserCredentialsHelper.FindUserRole(_context, User);
+                return View(await _context.Specialities.ToListAsync());
+            }
+
+            // GET: Roles/Details/5
+            public async Task<IActionResult> Details(int? id)
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var speciality = await _context.Specialities
+                    .FirstOrDefaultAsync(m => m.Id == id);
+                if (speciality == null)
+                {
+                    return NotFound();
+                }
+
+                return View(speciality);
+            }
+
+            // GET: Roles/Create
+            public IActionResult Create()
+            {
+                return View();
+            }
+
+            // POST: Roles/Create
+            // To protect from overposting attacks, enable the specific properties you want to bind to.
+            // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Create([Bind("Name,Id")] Speciality speciality)
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(speciality);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(speciality);
+            }
+
+            // GET: Roles/Edit/5
+            public async Task<IActionResult> Edit(int? id)
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var role = await _context.Roles.FindAsync(id);
+                if (role == null)
+                {
+                    return NotFound();
+                }
+                return View(role);
+            }
+
+            // POST: Roles/Edit/5
+            // To protect from overposting attacks, enable the specific properties you want to bind to.
+            // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Edit(int id, [Bind("Name,Id")] Speciality speciality)
+            {
+                if (id != speciality.Id)
+                {
+                    return NotFound();
+                }
+
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        _context.Update(speciality);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!SpecialitiesExists(speciality.Id))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(speciality);
+            }
+
+            // GET: Roles/Delete/5
+            public async Task<IActionResult> Delete(int? id)
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var role = await _context.Specialities
+                    .FirstOrDefaultAsync(m => m.Id == id);
+                if (role == null)
+                {
+                    return NotFound();
+                }
+
+                return View(role);
+            }
+
+            // POST: Roles/Delete/5
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> DeleteConfirmed(int id)
+            {
+                var role = await _context.Roles.FindAsync(id);
+                _context.Roles.Remove(role);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            private bool SpecialitiesExists(int id)
+            {
+                return _context.Specialities.Any(e => e.Id == id);
+            }
+        }
+}
---
+++ HospitalProject/Controllers/WardsController.cs
@@ -0,0 +1,121 @@
+﻿using Data.Entities;
+using Data.HospitalCountext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalProject.Controllers
+{
+    public class WardsController : Controller
+    {
+         private readonly HospitalContext _context;
+
+        public WardsController(HospitalContext context)
+        {
+            _context= context;
+        }
+        // GET: WardsController
+        public async Task<ActionResult> Index()
+        {
+            return View(await _context.Wards.ToListAsync());
+        }
+
+        // GET: WardsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Description,Id")] Ward ward)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ward);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ward);
+        }
+              
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ward = await _context.Wards.FindAsync(id);
+            if (ward == null)
+            {
+                return NotFound();
+            }
+            return View(ward);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Description,Id")] Ward ward)
+        {
+            if (id != ward.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ward);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WardExists(ward.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ward);
+        }
+
+        // GET: WardsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WardsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        private bool WardExists(int id)
+        {
+            return _context.Wards.Any(e => e.Id == id);
+        }
+    }
+}
